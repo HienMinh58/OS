b3e4d9490f7c6bfdd1ffb6e5031c1b7c8d478c9d
long <long.duongbaolong@hcmut.edu.vn>
2025-03-22 11:31:18 +0700

    Update queue.c
    hàm dequeue  return a pcb whose prioprity is the highest
    
    Co-Authored-By: HienMinh58 <151639433+HienMinh58@users.noreply.github.com>

--- src/queue.c
+++ src/queue.c
@@ -13,7 +13,7 @@ void enqueue(struct queue_t *q, struct pcb_t *proc)
 {
         /* TODO: put a new process to queue [q] */
         if (q == NULL || q->size >= MAX_QUEUE_SIZE)
-                return; // Kiểm tra NULL và quá giới hạn
+                return;
         q->proc[q->size++] = proc;
 }
 
@@ -21,27 +21,23 @@ struct pcb_t *dequeue(struct queue_t *q)
 {
         /* TODO: return a pcb whose prioprity is the highest in the queue [q] and remember to remove it from q
          * */
-        if (q == NULL || q->size == 0)
-                return NULL; // Kiểm tra NULL và hàng đợi rỗng
-
+        if (q == NULL || q->size == 0) return NULL;
         int highest_prioprity = 0;
         for (int i = 1; i < q->size; i++)
         {
                 if (q->proc[i]->priority < q->proc[highest_prioprity]->priority)
                 {
-                        highest_prioprity = i; // Tìm tiến trình có độ ưu tiên cao nhất
+                        highest_prioprity = i;
                 }
         }
 
         struct pcb_t *max_proc = q->proc[highest_prioprity];
-
-        // Dịch chuyển các phần tử còn lại lên trên
         for (int i = highest_prioprity; i < q->size - 1; i++)
         {
                 q->proc[i] = q->proc[i + 1];
         }
 
-        q->proc[q->size - 1] = NULL; // Xóa phần tử cuối
+        q->proc[q->size - 1] = NULL;
         q->size--;
         return max_proc;
 }
